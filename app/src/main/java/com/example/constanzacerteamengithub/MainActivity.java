d57eede0231a2c511c7567965607ac59de896f83
RachelWindforce <conyperla1818@gmail.com>
2024-11-12 15:51:40 -0300

    codigo final

--- app/src/main/java/com/example/constanzacerteamengithub/MainActivity.java
+++ app/src/main/java/com/example/constanzacerteamengithub/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner sexo;
+    //Spinner sexo;
     EditText peso, estatura;
     Button calcular;
     TextView imc, mensaje;
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sexo = findViewById(R.id.BoxSexo);
+        //sexo = findViewById(R.id.BoxSexo);
         peso = findViewById(R.id.txtPeso);
         estatura = findViewById(R.id.txtEstatura);
         calcular = findViewById(R.id.btnCalcular);
@@ -34,16 +34,37 @@ public class MainActivity extends AppCompatActivity {
 
         String Pesso = peso.getText().toString();
         String Esttatura = estatura.getText().toString();
-        String Mensaje = mensaje.getText().toString();
+
 
         if (Pesso.isEmpty() || Esttatura.isEmpty() ){
             Toast.makeText(this, "Porfavor complete todos los campos", Toast.LENGTH_SHORT).show();
 
         }else{
 
-            Double Peso = Double.parseDouble(String.valueOf(peso))/100;
-            Double Estatura = Double.parseDouble(String.valueOf(estatura));
+            Double Estatura = Double.parseDouble(Esttatura)/100.0;
+            Double Peso = Double.parseDouble(Pesso);
             Double IMC = Peso/(Estatura*Estatura);
+            imc.setText(IMC.toString().substring(0,5));
+
+            if (IMC > 25 ){
+                mensaje.setText("Como reducir el IMC:  \n 1)Haga 30 minutos de ejercicio aeróbico 5 veces por semana. " +
+                        "\n 2)Logre un equilibrio entre el consumo de calorías y la actividad física. " +
+                        "\n 3)Limite las grasas saturadas a no mas del 7% de las calorias totales " +
+                        "\n 4)Tenga una alimentacion baja en colesterol con carnes magras, frutas,verduras y cereales integrales. ");
+
+
+            } else if (IMC < 18.5) {
+                mensaje.setText("Como aumentar el IMC: \n 1) Come con mas frecuencia. Empieza poco a poco a hacer 5 a 6 comidas más pequeñas durante el dia" +
+                        "\n 2) Elige alimentos con muchos nutrientes." +
+                        "\n 3) Agrega aderezos." +
+                        "\n 4) Prueba licuados y batidos de frutas." +
+                        "\n 5) Controla que bebes y cuando." +
+                        "\n 6) Haz ejercicio.");
+
+            }else{
+                mensaje.setText("FELICITACIONES ud mantiene una exelente alimentacion");
+
+            }
 
         }
 
